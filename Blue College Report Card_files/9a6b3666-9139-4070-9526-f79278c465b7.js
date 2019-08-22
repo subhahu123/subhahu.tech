0a69daf4f12bbc5aa71c8cb043cd7f3aa30bed8a
subhahu123 <jainsubhahu.gmail.com>
2019-08-23 00:19:48 +0530

    adding new courses

---
+++ Blue College Report Card_files/9a6b3666-9139-4070-9526-f79278c465b7.js
@@ -0,0 +1,8 @@
+!function(){
+window.initialUsersnapClassicGetConfig={"apiKey": "9a6b3666-9139-4070-9526-f79278c465b7", "onloadCallbackName": "_d6912278-e381-4d5f-96ba-1d13c8c39e9b", "locale": "auto", "fields": {"comment": null, "assignee": null, "title": null, "email": null, "label": null}, "button": null, "shortcut": null, "apiAccess": "basic", "colors": {"primary": "#005E99", "secondary": "#66AACB"}, "serviceUrl": "https://api.usersnap.com", "consoleRecorder": false};
+var s=document.createElement('script'),n=document.getElementsByTagName('script')[0],p="//cdn.usersnap.com/classic/frontend";
+s.type='text/javascript';
+s.async=!0;
+s.src=p+"/widget/loader.js";
+n.parentNode.insertBefore(s,n);
+}();
