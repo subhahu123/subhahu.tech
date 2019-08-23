567841828b2c7d74eac28756fa5865cbda3d9bc4
subhahu123 <jainsubhahu.gmail.com>
2019-08-24 01:14:51 +0530

    fixing bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
-    $("#myDIV *").filter(function() {
+    $(".card").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
